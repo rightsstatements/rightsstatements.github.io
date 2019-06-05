9d8800a2abf6146a7b5eb97387e007c656b69cf4
Daniel Doblado <danieldoblado@gmail.com>
2019-06-05 12:48:37 +0200

    Add script to write NetlifyCMS config

---
+++ scripts/writeConf.js
@@ -0,0 +1,21 @@
+const fs = require("fs")
+const { promisify } = require('util')
+const writeFile = promisify(fs.writeFile)
+const readFile = promisify(fs.readFile)
+
+const writeConf = async () => {
+  const { CMS_REPO, CMS_BRANCH } = process.env
+
+  if (CMS_REPO && CMS_BRANCH) {
+    const config = await readFile('./admin/config.yml', 'utf8')
+    let replacedConfig = config
+      .replace('${CMS_REPO}', CMS_REPO)
+      .replace('${CMS_BRANCH}', CMS_BRANCH)
+    await writeFile('./admin/config.yml', replacedConfig)
+    console.info("Configuration replaced")
+  } else {
+    console.info("The configuration was not replaced")
+  }
+}
+
+writeConf()
