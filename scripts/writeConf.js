5dcb67c810015332156dc69504447e70a7f19a7e
Felix Ostrowski <felix.ostrowski@googlemail.com>
2019-06-12 11:16:46 +0200

    Configure site_domain, site_url and display_url

--- scripts/writeConf.js
+++ scripts/writeConf.js
@@ -4,13 +4,15 @@ const writeFile = promisify(fs.writeFile)
 const readFile = promisify(fs.readFile)
 
 const writeConf = async () => {
-  const { CMS_REPO, CMS_BRANCH } = process.env
+  const { CMS_REPO, CMS_BRANCH, CMS_DOMAIN, CMS_PROTO } = process.env
 
-  if (CMS_REPO && CMS_BRANCH) {
+  if (CMS_REPO && CMS_BRANCH && CMS_DOMAIN && CMS_PROTO) {
     const config = await readFile('./admin/config.yml', 'utf8')
     let replacedConfig = config
-      .replace('${CMS_REPO}', CMS_REPO)
-      .replace('${CMS_BRANCH}', CMS_BRANCH)
+      .replace(/\${CMS_REPO}/g, CMS_REPO)
+      .replace(/\${CMS_BRANCH}/g, CMS_BRANCH)
+      .replace(/\${CMS_DOMAIN}/g, CMS_DOMAIN)
+      .replace(/\${CMS_PROTO}/g, CMS_PROTO)
     await writeFile('./admin/config.yml', replacedConfig)
     console.info("Configuration replaced")
   } else {
