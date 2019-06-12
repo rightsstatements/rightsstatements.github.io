77d1679ae3fdd1f7eaaae3622c3cdc3d87f40794
Felix Ostrowski <felix.ostrowski@googlemail.com>
2019-06-12 11:16:46 +0200

    Configure site_domain

--- scripts/writeConf.js
+++ scripts/writeConf.js
@@ -4,13 +4,14 @@ const writeFile = promisify(fs.writeFile)
 const readFile = promisify(fs.readFile)
 
 const writeConf = async () => {
-  const { CMS_REPO, CMS_BRANCH } = process.env
+  const { CMS_REPO, CMS_BRANCH, CMS_DOMAIN } = process.env
 
-  if (CMS_REPO && CMS_BRANCH) {
+  if (CMS_REPO && CMS_BRANCH && CMS_DOMAIN) {
     const config = await readFile('./admin/config.yml', 'utf8')
     let replacedConfig = config
       .replace('${CMS_REPO}', CMS_REPO)
       .replace('${CMS_BRANCH}', CMS_BRANCH)
+      .replace('${CMS_DOMAIN}', CMS_DOMAIN)
     await writeFile('./admin/config.yml', replacedConfig)
     console.info("Configuration replaced")
   } else {
