71ebeedc8ddf22f03930d1be71b7841ed3c9c989
Felix Ostrowski <felix.ostrowski@googlemail.com>
2019-06-12 15:00:42 +0200

    Configure redirects

--- scripts/writeConf.js
+++ scripts/writeConf.js
@@ -1,23 +1,32 @@
 const fs = require("fs")
+const url = require("url")
 const { promisify } = require('util')
 const writeFile = promisify(fs.writeFile)
 const readFile = promisify(fs.readFile)
 
 const writeConf = async () => {
-  const { CMS_REPO, CMS_BRANCH, CMS_DOMAIN, CMS_PROTO } = process.env
+  const { CMS_REPO, CMS_BRANCH, DEPLOY_URL, URL } = process.env
 
-  if (CMS_REPO && CMS_BRANCH && CMS_DOMAIN && CMS_PROTO) {
-    const config = await readFile('./admin/config.yml', 'utf8')
-    let replacedConfig = config
+  if (CMS_REPO && CMS_BRANCH) {
+    const netlifyCmsConfig = await readFile('./admin/config.yml', 'utf8')
+    await writeFile('./admin/config.yml', netlifyCmsConfig
       .replace(/\${CMS_REPO}/g, CMS_REPO)
       .replace(/\${CMS_BRANCH}/g, CMS_BRANCH)
-      .replace(/\${CMS_DOMAIN}/g, CMS_DOMAIN)
-      .replace(/\${CMS_PROTO}/g, CMS_PROTO)
-    await writeFile('./admin/config.yml', replacedConfig)
+    )
     console.info("Configuration replaced")
   } else {
     console.info("The configuration was not replaced")
   }
+
+  const netlifyConfig = await readFile('./netlify.toml', 'utf8')
+  const X_FORWARDED_URL = url.parse(DEPLOY_URL || URL)
+  console.log(X_FORWARDED_URL)
+  await writeFile('./netlify.toml', netlifyConfig
+    .replace(/\${X_FORWARDED_HOST}/g, X_FORWARDED_URL.host)
+    .replace(/\${X_FORWARDED_PROTO}/g, X_FORWARDED_URL.protocol.slice(0, -1))
+  )
+  const updated = await readFile('./netlify.toml', 'utf8')
+  console.log("Updated", updated)
 }
 
 writeConf()
